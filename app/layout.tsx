f73c7d77f71dc05dc1ad1cdc68fe1e9ca88fe869
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-10-27 22:55:03 -0300

    Update metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Emilia & Manuel - Comida Saludable',
+  title: 'Apacha - Cocina consciente',
   description: 'Descubre el sabor del bienestar con nuestras comidas nutritivas y deliciosas',
 }
 
