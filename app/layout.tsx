02981f9d5243a9e925968cb23b641d046c0a6adf
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-11-24 23:22:48 -0300

    update font to roboto

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: ["300", "500", "700"],
+  style: "normal",
+  subsets: ["latin"]
+});
 
 export const metadata: Metadata = {
   title: 'Apacha - Cocina consciente',
   description: 'Descubre el sabor del bienestar con nuestras comidas nutritivas y deliciosas',
-}
+} 
 
 export default function RootLayout({
   children,
@@ -16,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   )
 }
