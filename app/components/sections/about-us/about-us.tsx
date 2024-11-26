e5976562d4c7de2ad20d8fb7f7dfba83a801da00
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-11-26 00:30:30 -0300

    Update about us section with new phrases

--- app/components/sections/about-us/about-us.tsx
+++ app/components/sections/about-us/about-us.tsx
@@ -9,7 +9,7 @@ export default function AboutUs() {
   return (
     <section id="sobre-nosotros" className="py-20 bg-gradient-to-b">
       <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold text-center mb-12 text-apacha-green">Conócenos</h2>
+        <h2 className="text-3xl font-bold text-center mb-12 text-apacha-green">Quiénes somos</h2>
         <Card className="overflow-hidden">
           <CardContent className="p-0 h-">
             <div className="flex flex-col md:flex-row">
@@ -24,13 +24,16 @@ export default function AboutUs() {
               <div className="md:w-2/3 p-6 md:p-8">
                 <h3 className="text-2xl font-semibold mb-4 text-apacha-brown">Emi y Manu</h3>
                 <p className="text-apacha-black mb-4">
-                  Juntos, Emi y Manu forman el corazón y el alma de Apacha. Con su pasión compartida por la cocina consciente y plant-based, han creado un espacio donde la innovación culinaria se encuentra con la sostenibilidad.
+                  Somos Emi y Manu, hermanos mellizos, que además de tener un vínculo muy especial desde siempre, hoy nos encontramos compartiendo y emprendiendo en Apacha.
                 </p>
                 <p className="text-apacha-black mb-4">
-                  Emi, con su experiencia en nutrición, asegura que cada plato sea tan nutritivo como delicioso. Manu, por su parte, aporta su creatividad culinaria, transformando ingredientes simples en experiencias gastronómicas únicas.
+                  Tenemos 29 años. Manu es biólogo y Emi estudiante avanzada de Nutrición, pero independientemente de nuestras formaciones, la cocina siempre ha estado muy presente en nuestra vida. En nuestra familia, el alimento siempre ha generado momentos de encuentros, disfrute y alegrías. Lo vivimos en definitiva, como una gran forma de apapachar.
                 </p>
                 <p className="text-apacha-black">
-                  Juntos, están redefiniendo lo que significa comer de manera consciente y deliciosa, invitándote a unirte a su viaje hacia un estilo de vida más saludable y sostenible.
+                  A partir de ahí surgen estas ganas de emprender en conjunto. Con una gran complementariedad entre nosotrxs; Emi con una fuerte convicción en la alimentación consciente, intuitiva, basada en plantas y placentera. Manu con la pasión desde niño por el mundo de la gastronomía y su gran habilidad de cocinar tan rico.
+                </p>
+                <p className="text-apacha-black">
+                  Nuestro gran objetivo es poder compartir el disfrute por nuevas alternativas gastronomicas y poder demostrar que comer rico y de forma consciente es posible.
                 </p>
               </div>
             </div>
