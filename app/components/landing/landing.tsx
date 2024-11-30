496486b2a0db779d93ec1b0c3dafbc3b1de38606
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-11-30 08:36:00 -0300

    Update hero banner images

--- app/components/landing/landing.tsx
+++ app/components/landing/landing.tsx
@@ -35,10 +35,9 @@ export default function Landing() {
   }, [api]);
 
   const carouselImages = [
-    '/hero-1.jpg',
-    '/hero-2.jpg',
     '/hero-3.jpg',
     '/hero-4.jpg',
+    '/hero-5.jpg',
   ];
 
   const ourProducts = [{
