727d21167951f6f2ea8cb814d2a68aef9cb38452
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-12-01 11:01:04 -0300

    Contact us section

--- app/components/sections/contact-us/contact-us.tsx
+++ app/components/sections/contact-us/contact-us.tsx
@@ -1,27 +1,86 @@
+'use client'
+
 import React from 'react'
-import Link from 'next/link';
+import Link from 'next/link'
+import { motion } from 'framer-motion'
+import { Instagram, Mail, Phone, MapPin } from 'lucide-react'
 
 export default function ContactUs() {
   return (
-    <section id="contacto" className="bg-brown-700 text-white py-20">
-      <div className="container mx-auto px-4">
+    <section id="contacto" className=" py-20 relative overflow-hidden">
+      <motion.div
+        className="absolute inset-0 z-0"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+      >
+        {[...Array(5)].map((_, index) => (
+          <motion.div
+            key={index}
+            className="absolute rounded-full bg-bone-800 opacity-10"
+            style={{
+              width: Math.random() * 100 + 50,
+              height: Math.random() * 100 + 50,
+              left: `${Math.random() * 100}%`,
+              top: `${Math.random() * 100}%`,
+            }}
+            animate={{
+              y: [0, -20, 0],
+              x: [0, 10, 0],
+              scale: [1, 1.1, 1],
+            }}
+            transition={{
+              duration: Math.random() * 5 + 5,
+              repeat: Infinity,
+              repeatType: "reverse",
+            }}
+          />
+        ))}
+      </motion.div>
+
+      <div className="container mx-auto px-4 relative z-10">
         <h2 className="text-3xl font-bold text-center mb-12">Contáctanos</h2>
-        <div className="flex flex-col md:flex-row justify-center items-center md:space-x-12">
-          <div className="mb-8 md:mb-0 flex items-center">
+        <div className="flex flex-col md:flex-row justify-center items-center md:space-x-8 space-y-6 md:space-y-0">
+          <motion.div
+            className="flex items-center space-x-2"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Mail className="w-5 h-5" />
             <span>contacto@apacha.com.uy</span>
-          </div>
-          <div className="mb-8 md:mb-0 flex items-center">
+          </motion.div>
+          <motion.div
+            className="flex items-center space-x-2"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Phone className="w-5 h-5" />
             <Link target="_blank" href="https://wa.me/59898958230?text=Quiero%20saber%20mas%20sobre%20sus%20productos">
               <span>+598 098 958 230</span>
             </Link>
-          </div>
-          <div className="flex items-center">
+          </motion.div>
+          <motion.div
+            className="flex items-center space-x-2"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <MapPin className="w-5 h-5" />
             <Link target="_blank" href="https://www.google.com/maps/place/Apacha/@-34.8876343,-56.1543239,17z/data=!4m16!1m9!3m8!1s0x959f81897c9e77e5:0xa3c2343cf687d69b!2sApacha!8m2!3d-34.8876387!4d-56.151749!9m1!1b1!16s%2Fg%2F11td4cfrs3!3m5!1s0x959f81897c9e77e5:0xa3c2343cf687d69b!8m2!3d-34.8876387!4d-56.151749!16s%2Fg%2F11td4cfrs3?entry=ttu&g_ep=EgoyMDI0MTAyMy4wIKXMDSoASAFQAw%3D%3D">
               <span>Dr. Pedro Escuder Núñez 2245-2199, 11600 Montevideo</span>
             </Link>
-          </div>
+          </motion.div>
+          <motion.div
+            className="flex items-center space-x-2"
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 0.95 }}
+          >
+            <Instagram className="w-5 h-5" />
+            <Link target="_blank" href="https://www.instagram.com/apacha.uy/">
+              <span>@apacha.uy</span>
+            </Link>
+          </motion.div>
         </div>
       </div>
     </section>
-  );
-};
+  )
+}
