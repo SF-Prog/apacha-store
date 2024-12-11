4b532f4c4577ef0cc06e266d356b888731db6952
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-12-11 02:23:15 -0300

    Adjust images

--- app/menu/page.tsx
+++ app/menu/page.tsx
@@ -1,12 +1,14 @@
 'use client'
+
 import React , { useState} from "react"
 import { motion } from 'framer-motion'
-import { Leaf, Truck } from 'lucide-react'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { Leaf, Truck, Recycle } from 'lucide-react'
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 import SecondaryPageLayout from "@/components/secondary-page/layout/layout";
 import PageHeader from "@/components/secondary-page/page-header/page-header";
 import PageContent from "@/components/secondary-page/page-content/page-content"
 import MakeYourOrder from "../components/sections/make-your-order/make-your-order"
+import ScrollAnimatedBackground from "../components/landing/scroll-animation/scroll-animation"
 
 export default function MenuPage() {
   const [selectedMeals, setSelectedMeals] = useState<string[]>([])
@@ -62,74 +64,92 @@ export default function MenuPage() {
   return (
     <SecondaryPageLayout>
       <PageHeader
-        title="Menu Semanal"
-        description="Descubre nuestra selección de comidas saludables y deliciosas"
-        imageSrc="/products-dips-2.jpg"
+        title="Nuestro plato principal"
+        description="Descubre nuestra nuestro menu de viandas"
+        imageSrc="/hero-4.jpg"
       />
       <PageContent>
-        <div className="bg-apacha-beige min-h-screen py-12 px-4 sm:px-6 lg:px-8">
-          <motion.div 
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 0.8 }}
-            className="max-w-4xl mx-auto"
-          >
-            <h1 className="text-4xl font-bold text-apacha-brown text-center mb-8">Nuestro Menú Semanal</h1>
-            <Card className="mb-8">
-              <CardHeader>
-                <CardTitle className="flex items-center text-2xl text-apacha-green">
-                  <Truck className="mr-2" />
-                  Cómo Funciona Nuestro Servicio
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="text-apacha-black">
-                <p className="mb-4">Ofrecemos un menú semanal con entregas dos veces por semana para asegurar la frescura de nuestros platos:</p>
-                <ul className="list-disc list-inside mb-4">
-                  <li><strong>Domingo:</strong> Entregamos 3 almuerzos (para Lunes, Martes y Miércoles)</li>
-                  <li><strong>Miércoles:</strong> Entregamos 2 almuerzos (para Jueves y Viernes)</li>
-                </ul>
-                <p className="mb-4">Las entregas se realizan entre las 17:00 y las 20:00 horas, utilizando nuestro propio método de entrega para garantizar que la comida llegue segura a tu puerta.</p>
-              </CardContent>
-            </Card>
+        <ScrollAnimatedBackground
+          imageSrc="/lunch-table.jpg"
+          imageAlt="lunch-table">
+          <div className="bg-apacha-beige min-h-screen py-12 px-4 sm:px-6 lg:px-8">
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.8 }}
+              className="max-w-4xl mx-auto"
+            >
+              <h1 className="text-4xl font-bold text-apacha-brown text-center mb-8">Menú Semanal</h1>
+              <Card className="mb-8">
+                <CardHeader>
+                  <CardTitle className="flex items-center text-2xl text-apacha-green">
+                    <Truck className="mr-2" />
+                    Entregas
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="text-apacha-black">
+                  <p className="mb-4">Ofrecemos un menú semanal con entregas dos veces por semana para asegurar la frescura de nuestros platos:</p>
+                  <ul className="list-disc list-inside mb-4">
+                    <li><strong>Domingo:</strong> Entregamos 3 almuerzos (para Lunes, Martes y Miércoles)</li>
+                    <li><strong>Miércoles:</strong> Entregamos 2 almuerzos (para Jueves y Viernes)</li>
+                  </ul>
+                  <p className="mb-4">Las entregas se realizan entre las 17:00 y las 20:00 horas, utilizando nuestro propio método de entrega para garantizar que la comida llegue segura a tu puerta.</p>
+                </CardContent>
+              </Card>
 
-            <Card className="mb-8">
-              <CardHeader>
-                <CardTitle className="flex items-center text-2xl text-apacha-green">
-                  <Leaf className="mr-2" />
-                  Nuestro Compromiso
-                </CardTitle>
-              </CardHeader>
-              <CardContent className="text-apacha-black">
-                <p>Todas nuestras comidas son libres de gluten y basadas en alimentos de la tierra. Este desafío es la razón por la que nuestros platos contienen tantos nutrientes y sabores innovadores. Nos esforzamos por crear opciones saludables y deliciosas que satisfagan todas las necesidades dietéticas.</p>
-              </CardContent>
-            </Card>
+              <Card className="mb-8">
+                <CardHeader>
+                  <CardTitle className="flex items-center text-2xl text-apacha-green">
+                    <Leaf className="mr-2" />
+                    Nuestra motivacion
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="text-apacha-black">
+                  <p>Todas nuestras comidas son libres de gluten y basadas en alimentos de la tierra. Este desafío es la razón por la que nuestros platos contienen tantos nutrientes y sabores innovadores. Nos esforzamos por crear opciones saludables y deliciosas que satisfagan todas las necesidades dietéticas.</p>
+                </CardContent>
+              </Card>
 
-            <h2 className="text-3xl font-semibold text-apacha-brown text-center mb-6">Ejemplo de Menú Semanal</h2>
-            
-            {weeklyMenu.map((item, index) => (
-              <motion.div
-                key={item.day}
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: index * 0.1 }}
-              >
-                <Card className="mb-4">
-                  <CardHeader>
-                    <CardTitle className="text-xl text-apacha-green">{item.day}: {item.dish}</CardTitle>
-                  </CardHeader>
-                  <CardContent className="text-apacha-black">
-                    <p>{item.description}</p>
-                  </CardContent>
-                </Card>
-              </motion.div>
-            ))}
-            <MakeYourOrder
-              selectedMeals={selectedMeals}
-              totalPrice={totalPrice}
-              mealPacks={mealPacks}
-              toggleMeal={toggleMeal} />
-          </motion.div>
-        </div>
+              <Card className="mb-8">
+                <CardHeader>
+                  <CardTitle className="flex items-center text-2xl text-apacha-green">
+                    <Recycle className="mr-2" />
+                    Compromiso Ambiental
+                  </CardTitle>
+                </CardHeader>
+                <CardContent className="text-apacha-black">
+                  <p>
+                    En Apacha, nos preocupamos por el medio ambiente tanto como por tu salud. Nuestros almuerzos se entregan en contenedores reutilizables diseñados para ser devueltos y reutilizados, contribuyendo así a la reducción de residuos plásticos. Estos contenedores, fabricados con plástico reciclado gracias a nuestra colaboración con RECICLATE, son aptos para microondas, permitiéndote calentar nuestros platos de manera segura y conveniente. Al devolver estos contenedores, estás participando activamente en nuestro esfuerzo por combatir la crisis ambiental. ¡Juntos, podemos hacer la diferencia!
+                  </p>
+                </CardContent>
+              </Card>
+
+              <h2 className="text-3xl font-semibold text-apacha-brown text-center mb-6">Ejemplo de Menú Semanal</h2>
+              
+              {weeklyMenu.map((item, index) => (
+                <motion.div
+                  key={item.day}
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.5, delay: index * 0.1 }}
+                >
+                  <Card className="mb-4">
+                    <CardHeader>
+                      <CardTitle className="text-xl text-apacha-green">{item.day}: {item.dish}</CardTitle>
+                    </CardHeader>
+                    <CardContent className="text-apacha-black">
+                      <p>{item.description}</p>
+                    </CardContent>
+                  </Card>
+                </motion.div>
+              ))}
+              <MakeYourOrder
+                selectedMeals={selectedMeals}
+                totalPrice={totalPrice}
+                mealPacks={mealPacks}
+                toggleMeal={toggleMeal} />
+            </motion.div>
+          </div>
+        </ScrollAnimatedBackground>
       </PageContent>
     </SecondaryPageLayout>
   )
