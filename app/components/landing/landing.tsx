50b6c4faad00b1024f79cc75c271c338bcc6a3d7
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-12-13 20:24:38 -0300

    Remove unnused code, update images and wording

--- app/components/landing/landing.tsx
+++ app/components/landing/landing.tsx
@@ -26,26 +26,10 @@ export default function Landing() {
   }, [api]);
 
   const carouselImages = [
-    '/hero-3.jpg',
     '/hero-4.jpg',
-    '/hero-5.jpg',
-  ];
-
-  const ourProducts = [{
-      image: '/desayunos-producto.png',
-      name: 'Desayunos Energizantes',
-      description: 'Empieza tu día con un estallido de frescura y sabor. Nuestros desayunos son jugos 100% naturales, llenos de frutas y vegetales frescos, cargados de vitaminas y antioxidantes para revitalizarte. Cada sorbo es un impulso energético y una manera deliciosa de cuidar tu cuerpo desde el amanecer. ¡Prueba uno y siente la diferencia!'
-    },
-    {
-      image: '/almuerzo-producto.png',
-      name: 'Almuerzos que Satisfacen',
-      description: 'Nuestros almuerzos son una celebración de sabores y nutrición. Platos veganos y libres de gluten que no solo llenan, sino que nutren con ingredientes cuidadosamente seleccionados. Desde ensaladas vibrantes hasta guisos reconfortantes, cada opción te deja saciado sin sentirte pesado, para que disfrutes tu día con energía renovada. ¡Ideal para probar la variedad y deleitar tu paladar!'
-    },
-    {
-      image: '/cena-producto.png',
-      name: 'Cenas Nutritivas y Livianas',
-      description: 'Termina tu jornada con nuestras sopas nutritivas, diseñadas para reconfortarte y nutrirte. Cada cena es ligera, pero cargada de ingredientes que tu cuerpo agradecerá al final del día. El equilibrio perfecto entre sabor y bienestar, para que te sientas satisfecho sin preocupaciones. Una opción que seguro querrás incorporar en tu rutina nocturna.'
-    }
+    '/catering-cake.jpg',
+    '/soup-hero.jpg',
+    '/breakfast-serving.png'
   ];
 
   return (
@@ -72,79 +56,22 @@ export default function Landing() {
           </Carousel>
           <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center">
             <div className="text-center text-bone-700 z-10">
-              <h2 className="text-4xl md:text-6xl font-bold mb-4 font-serif">
-                Cocina Consciente Plant Based
+              <h2 className="text-4xl md:text-6xl font-bold mb-4">
+                Disfruta de una alimentación nutritiva
               </h2>
               <p className="text-xl mb-8 font-sans">
-                Descubre el sabor y el bienestar con nuestros platos nutritivos y deliciosos, 100% vegetales
+                Todo lo que elaboramos es plant based y gluten free
               </p>
             </div>
           </div>
         </section>
-      <ScrollAnimatedBackground
-        imageSrc="/background-mid-section.png"
-        imageAlt="background-mid-section">
-        <AboutUs />
-        <AboutApacha />
-
-        {/* <section id="menú" className="bg-gradient-to-b py-20">
-          <div className="container mx-auto px-4">
-            <h2 className="text-3xl font-bold text-center mb-12 text-brown-700">Nuestros Platos Destacados</h2>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-              {renderProducts()}
-            </div>
-          </div>
-        </section> */}
-
-          <OurServices />
-          {/* <MakeYourOrder
-            mealPacks={mealPacks}
-            selectedMeals={selectedMeals}
-            toggleMeal={toggleMeal}
-            totalPrice={totalPrice} /> */}
+        <ScrollAnimatedBackground
+          imageSrc="/background-mid-section.png"
+          imageAlt="background-mid-section">
+            <AboutUs />
+            <AboutApacha />
+            <OurServices />
         </ScrollAnimatedBackground>
-        {/* <section id="eventos" className="py-20 bg-beige-100">
-          <div className="container mx-auto px-4">
-            <h2 className="text-3xl font-bold text-center mb-12 text-brown-700">Nuestros Eventos</h2>
-            <div className="mb-16">
-              <h3 className="text-2xl font-semibold mb-6 text-brown-600">Próximos Eventos</h3>
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-                {[
-                  { 
-                    titulo: 'Taller de Cocina Saludable', 
-                    fecha: '15 de Noviembre, 2023', 
-                    hora: '18:00 - 20:00',
-                    lugar: 'Nuestra Cocina Central'
-                  },
-                  { 
-                    titulo: 'Cena Degustación', 
-                    fecha: '3 de Diciembre, 2023', 
-                    hora: '20:30 - 23:00',
-                    lugar: 'Restaurante El Jardín'
-                  },
-                  { 
-                    titulo: 'Mercado de Productos Orgánicos', 
-                    fecha: '10 de Diciembre, 2023', 
-                    hora: '10:00 - 14:00',
-                    lugar: 'Plaza Mayor'
-                  }
-                ].map((evento) => (
-                  <Card key={evento.titulo}>
-                    <CardHeader>
-                      <CardTitle>{evento.titulo}</CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                      <p className="mb-2">{evento.fecha}</p>
-                      <p className="mb-2">{evento.hora}</p>
-                      <p>{evento.lugar}</p>
-                    </CardContent>
-                  </Card>
-                ))}
-              </div>
-            </div>
-          </div>
-        </section>
-         */}
         <ContactUs />
       </main>
     </Layout>
