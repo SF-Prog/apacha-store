f3cd6c535b9963999f6468fcb2c728b762e83816
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-12-14 18:35:35 -0300

    add product item type and align others

--- app/types/index.d.ts
+++ app/types/index.d.ts
@@ -1,8 +1,30 @@
 interface MealPack {
-  id: string,
-  title: string,
-  image: string,
-  description: string,
-  price: number,
+  id: string
+  title: string
+  image: string
+  description: string
+  price: number
   whatsappLink: string
+}
+
+interface ProductItem {
+  id: string
+  title: string
+  image: string
+  description?: string
+  price: number
+}
+
+interface CartItem extends ProductItem {
+  quantity: number
+}
+
+interface StoreContextType {
+  products: ProductItem[],
+  cartItems: CartItem[]
+  total: number
+  addCartItem: (item: ProductItem) => void
+  removeCartItem: (itemId: string) => void
+  emptyCart: () => void
+  updateCartFromStorage: () => void
 }
