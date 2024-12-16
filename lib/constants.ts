fb5e39619ed803a4cfbe0ad1cc14ce902b41a929
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2024-12-16 01:07:38 -0300

    Create new contants file and configure path alias

---
+++ lib/constants.ts
@@ -0,0 +1,354 @@
+const products: ProductListByCategory[] = [
+  {
+    name: "PANIFICADOS",
+    products: [
+      {
+        id: "focaccia-grande",
+        title: "Focaccia Grande",
+        image: "/placeholder.svg",
+        description: "Cebolla colorada, romero, sal gruesa y aceite de oliva / zucchini, ajo seco, tomillo, sal gruesa y aceite de oliva",
+        price: 350,
+        meassure: "950 g"
+      },
+      {
+        id: "focaccia-chica",
+        title: "Focaccia Chica",
+        image: "/placeholder.svg",
+        description: "Cebolla colorada, romero, sal gruesa y aceite de oliva / zucchini, ajo seco, tomillo, sal gruesa y aceite de oliva",
+        price: 300,
+        meassure: "500 g"
+      },
+      {
+        id: "pan-de-semillas",
+        title: "Pan de semillas",
+        image: "/placeholder.svg",
+        price: 320,
+        meassure: "900 g"
+      },
+      {
+        id: "crackers-de-colores",
+        title: "Crackers de colores",
+        image: "/placeholder.svg",
+        description: "Espinaca, remolacha y cúrcuma",
+        price: 190,
+        meassure: "170 g"
+      },
+      {
+        id: "galletitas-de-lino",
+        title: "Galletitas de lino",
+        image: "/placeholder.svg",
+        price: 190,
+        meassure: "170 g"
+      },
+      {
+        id: "pan-de-campo",
+        title: "Pan de campo",
+        image: "/placeholder.svg",
+        price: 330,
+        meassure: "750 g"
+      },
+      {
+        id: "pan-ciabatta",
+        title: "Pan ciabatta",
+        image: "/placeholder.svg",
+        price: 220,
+        meassure: "4 unidades"
+      },
+      {
+        id: "baguette",
+        title: "Baguette",
+        image: "/placeholder.svg",
+        price: 180,
+        meassure: "330 g"
+      }
+    ]
+  },
+  {
+    name: "QUESOS Y UNTABLES",
+    products: [
+      {
+        id: "queso-untable-caju",
+        title: "Queso untable de cajú fermentado",
+        image: "/placeholder.svg",
+        price: 300,
+        meassure: "200 mL"
+      },
+      {
+        id: "dip-verde-caju",
+        title: "Dip verde de cajú",
+        image: "/placeholder.svg",
+        price: 300,
+        meassure: "200 mL"
+      },
+      {
+        id: "hummus-aceitunas-negras",
+        title: "Hummus de aceitunas negras",
+        image: "/placeholder.svg",
+        price: 300,
+        meassure: "200 mL"
+      },
+      {
+        id: "pate-girasol",
+        title: "Paté de girasol",
+        image: "/placeholder.svg",
+        price: 250,
+        meassure: "200 mL"
+      },
+      {
+        id: "babaganoush",
+        title: "Babaganoush",
+        image: "/placeholder.svg",
+        price: 260,
+        meassure: "200 mL"
+      },
+      {
+        id: "bocconcini",
+        title: "Bocconcini",
+        image: "/placeholder.svg",
+        price: 350,
+        meassure: "200 mL"
+      }
+    ]
+  },
+  {
+    name: "CONSERVAS",
+    products: [
+      {
+        id: "pickles-pepino-chico",
+        title: "Pickles de pepino chico",
+        image: "/placeholder.svg",
+        price: 250,
+        meassure: "200 mL"
+      },
+      {
+        id: "pickles-pepino-grande",
+        title: "Pickles de pepino grande",
+        image: "/placeholder.svg",
+        price: 320,
+        meassure: "660 mL"
+      }
+    ]
+  },
+  {
+    name: "TARTAS",
+    products: [
+      {
+        id: "tarta-boniato",
+        title: "Tarta de Boniato zanahoria, espinaca y nueces",
+        image: "/placeholder.svg",
+        price: 280,
+        meassure: "16 cm"
+      },
+      {
+        id: "tarta-calabacin",
+        title: "Tarta de Calabacín, cebolla caramelizada y almendras",
+        image: "/placeholder.svg",
+        price: 280,
+        meassure: "16 cm"
+      },
+      {
+        id: "tarta-verduras-grilladas",
+        title: "Tarta de Verduras grilladas",
+        image: "/placeholder.svg",
+        price: 280,
+        meassure: "16 cm"
+      },
+      {
+        id: "tarta-pascualina",
+        title: "Tarta Pascualina",
+        image: "/placeholder.svg",
+        price: 280,
+        meassure: "16 cm"
+      },
+      {
+        id: "tarta-tomates-bocconcini",
+        title: "Tarta de Tomates confitados, bocconcini y albahaca",
+        image: "/placeholder.svg",
+        price: 300,
+        meassure: "16 cm"
+      },
+      {
+        id: "tarta-tofu-hongos",
+        title: "Tarta de Tofu, hongos y cebolla colorada",
+        image: "/placeholder.svg",
+        price: 300,
+        meassure: "16 cm"
+      }
+    ]
+  },
+  {
+    name: "HAMBURGUESAS",
+    products: [
+      {
+        id: "hamburguesas-3",
+        title: "Hamburguesas (3 unidades)",
+        image: "/placeholder.svg",
+        description: "Quinoa, remolacha y nuez / Mijo, boniato zanahoria y coriandro / Porotos negros, zanahoria y laurel / Garbanzos, berenjenas asadas y tomates secos",
+        price: 250,
+        meassure: "3 unidades de 150 g"
+      },
+      {
+        id: "hamburguesas-6",
+        title: "Hamburguesas (6 unidades)",
+        image: "/placeholder.svg",
+        description: "Quinoa, remolacha y nuez / Mijo, boniato zanahoria y coriandro / Porotos negros, zanahoria y laurel / Garbanzos, berenjenas asadas y tomates secos",
+        price: 450,
+        meassure: "6 unidades de 150 g"
+      }
+    ]
+  },
+  {
+    name: "YOGUR DE COCO",
+    products: [
+      {
+        id: "yogur-coco-grande",
+        title: "Yogur de Coco Grande",
+        image: "/placeholder.svg",
+        description: "Natural / frutos rojos / mango",
+        price: 330,
+        meassure: "660 mL"
+      },
+      {
+        id: "yogur-coco-chico",
+        title: "Yogur de Coco Chico",
+        image: "/placeholder.svg",
+        description: "Natural / frutos rojos / mango",
+        price: 180,
+        meassure: "250 mL"
+      }
+    ]
+  },
+  {
+    name: "GRANOLA",
+    products: [
+      {
+        id: "granola-chica",
+        title: "Granola Chica",
+        image: "/placeholder.svg",
+        description: "Elaborada con avena fermentada y frutos secos activados",
+        price: 290,
+        meassure: "250 g"
+      },
+      {
+        id: "granola-grande",
+        title: "Granola Grande",
+        image: "/placeholder.svg",
+        description: "Elaborada con avena fermentada y frutos secos activados",
+        price: 420,
+        meassure: "400 g"
+      }
+    ]
+  },
+  {
+    name: "TORTAS",
+    products: [
+      {
+        id: "torta-apachate",
+        title: "Torta Apachate",
+        image: "/placeholder.svg",
+        description: "Bizcochuelo de almendras con buttercream y salsa de frutos rojos",
+        price: 1400,
+        meassure: "22 cm - 12 porciones"
+      },
+      {
+        id: "torta-cumpleanito",
+        title: "Torta Cumpleañito",
+        image: "/placeholder.svg",
+        description: "Bizcochuelo de almendras y vainilla, relleno de dulce de leche con buttercream y garrapiñada de nueces",
+        price: 1650,
+        meassure: "22 cm - 12 porciones"
+      },
+      {
+        id: "torta-chocolatosa",
+        title: "Torta Chocolatosa",
+        image: "/placeholder.svg",
+        description: "Bizcochuelo de cacao relleno de ganache de chocolate con avellanas caramelizadas",
+        price: 1650,
+        meassure: "22 cm - 12 porciones"
+      }
+    ]
+  },
+  {
+    name: "ALFAJORES",
+    products: [
+      {
+        id: "alfajor-frutos-rojos",
+        title: "Alfajor Frutos Rojos",
+        image: "/placeholder.svg",
+        description: "Tapas de cacao rellenas de ganache de chocolate con centro de frutos rojos con cobertura de chocolate y sal marina",
+        price: 160,
+        meassure: "100 g"
+      },
+      {
+        id: "alfajor-pistacho",
+        title: "Alfajor Pistacho",
+        image: "/placeholder.svg",
+        description: "Tapas de maní y cacao rellenas de ganache de chocolate con centro de pistacho",
+        price: 160,
+        meassure: "100 g"
+      },
+      {
+        id: "alfajor-sesamo",
+        title: "Alfajor Sésamo",
+        image: "/placeholder.svg",
+        description: "Tapas de sésamo rellenas de dulce de leche y tahini con cobertura de chocolate",
+        price: 160,
+        meassure: "100 g"
+      },
+      {
+        id: "alfajores-pack",
+        title: "Pack de Alfajores",
+        image: "/placeholder.svg",
+        description: "Pack de 6 alfajores variados",
+        price: 840,
+        meassure: "6 unidades"
+      }
+    ]
+  },
+  {
+    name: "BUDINES",
+    products: [
+      {
+        id: "budin-limon-arandanos",
+        title: "Budín de Limón y arándanos",
+        image: "/placeholder.svg",
+        price: 480
+      },
+      {
+        id: "budin-banana-chocolate",
+        title: "Budín de Banana y chips de chocolate",
+        image: "/placeholder.svg",
+        price: 480
+      },
+      {
+        id: "budin-naranja-chia",
+        title: "Budín de Naranja y chía",
+        image: "/placeholder.svg",
+        price: 480
+      },
+      {
+        id: "budin-cacao",
+        title: "Budín de Cacao",
+        image: "/placeholder.svg",
+        price: 480
+      }
+    ]
+  },
+  {
+    name: "ALFAJORCITOS",
+    products: [
+      {
+        id: "alfajorcitos-pack",
+        title: "Cajita de Alfajorcitos",
+        image: "/placeholder.svg",
+        description: "Cajita de 8 alfajorcitos (dos de cada sabor): Limón y coco, Sésamo y membrillo, Chocolate y mousse, Choco maní y toffee",
+        price: 550,
+        meassure: "8 unidades"
+      }
+    ]
+  }
+];
+
+export {
+  products
+}
