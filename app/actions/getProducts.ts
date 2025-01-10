19ad3b20d2a2e29019700766c2547c5fd3343fcd
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2025-01-10 01:16:47 -0300

    get products action

---
+++ app/actions/getProducts.ts
@@ -0,0 +1,13 @@
+import { supabase } from '@/lib/supabase';
+export async function getProducts() {
+  //  Fetch products data 
+  
+  const { data, error } = await supabase.rpc('get_products_grouped_by_category');
+
+  if (error) {
+    console.error('Error fetching products:', error);
+    return [];
+  };
+
+  return data;
+}
