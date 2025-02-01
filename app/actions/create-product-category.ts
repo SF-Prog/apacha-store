523fbcff9fb5a059b6bf380cc1129d8bd7d6f1e0
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2025-02-01 19:38:39 -0300

    CRUD category product action

---
+++ app/actions/create-product-category.ts
@@ -0,0 +1,29 @@
+import { supabase } from "@/lib/supabase";
+
+const defaultErrorMessage = 'Failed to create product category';
+
+export const createProductCategory = async (cat: ProductCategory) => {
+  try {
+    const newProductCat = { name: cat.name, created_at: new Date };
+
+    const { error } = await supabase.from('product_category')
+      .insert(newProductCat);
+
+    if (error) {
+      return {
+        success: false,
+        error: error?.message ?? defaultErrorMessage
+      };
+    };
+
+    return {
+      success: true,
+      message: 'Product Category successfully created'
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error?.message ?? defaultErrorMessage
+    }
+  }
+};
---
+++ app/actions/delete-product-category.ts
@@ -0,0 +1,29 @@
+import { supabase } from "@/lib/supabase";
+
+const defaultErrorMessage = 'Failed to delete product category';
+
+export const deleteProductCategory = async (id: string) => {
+  try {
+    const { error } = await supabase
+      .from('product_category')
+      .delete()
+      .eq('id', id);
+
+    if (error) {
+      return {
+        success: false,
+        error: error?.message ?? defaultErrorMessage
+      };
+    };
+
+    return {
+      success: true,
+      message: 'Product Category deleted successfully'
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error?.message ?? defaultErrorMessage
+    }
+  }
+};
