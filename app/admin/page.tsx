8a50e54b8bad55650866b66ae5740ce61472ed02
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2025-02-06 00:21:33 -0300

    add page session check and redirect accordingly

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,23 @@
 'use client'
 
-import React from "react";
+import React, { useEffect } from "react";
 import AdminPanel from "@/components/admin-panel/layout/layout";
-import { supabase } from "@/lib/supabase";
+import { useAuth } from "@/context/auth-context";
+import { useRouter } from "next/navigation";
+
 
 export default function AdminPage() {
+  const { isAdmin } = useAuth();
+  const router = useRouter();
+
+  const redirectIfNotAdmin = async () => {
+    const isAuthenticated = await isAdmin();
+    if (!isAuthenticated)  return router.push('/admin/login');
+  };
+
+  useEffect(() => {
+    void redirectIfNotAdmin();
+  }, []);
 
   return (
     <AdminPanel />
