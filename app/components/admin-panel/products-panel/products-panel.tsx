5bc75e66a45c814bc3edd4b99b0c497b78535962
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2025-02-11 01:11:50 -0300

    Update products panel to display all data

--- app/components/admin-panel/products-panel/products-panel.tsx
+++ app/components/admin-panel/products-panel/products-panel.tsx
@@ -6,7 +6,7 @@ import { Button } from '@/components/ui/button'
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { useAdmin } from '@/context/admin-context'
-import { AddProductForm } from '@/components/admin-panel/add-product-form/add-product-form';
+import { ProductForm } from '@/components/admin-panel/product-form/product-form';
 
 export function ProductsPanel() {
   const {
@@ -14,18 +14,21 @@ export function ProductsPanel() {
     setShowCreateProductModal,
     productsList,
     removeProduct,
+    addProduct,
+    editProduct,
+    productCategories
   } = useAdmin();
 
   const [showEditProductModal, setShowEditProductModal] = useState(false);
+  const [productToEdit, setProductToEdit] = useState<ProductItem|null>(null);
 
   const handleRemoveProduct = (id: string) => {
     removeProduct(id);
   };
 
-  const onEdit = (product: ProductItem) => {
+  const handleEdit = (product: ProductItem) => {
     setShowEditProductModal(true);
-    // You might want to set the product to edit in state here
-    // setProductToEdit(product);
+    setProductToEdit(product);
   };
 
   const renderProductImage = (image: string, title: string) => {
@@ -42,7 +45,8 @@ export function ProductsPanel() {
   const renderProductsList = () => {
     if (!productsList?.length) return;
 
-    return productsList.map((product, index) => {
+    return productsList.map((product) => {
+      const category = productCategories.find(p => p.id === product.category);
       if (!product) return;
       return (
         <TableRow key={product.id}>
@@ -51,21 +55,23 @@ export function ProductsPanel() {
           </TableCell>
           <TableCell className="font-medium">{product.title}</TableCell>
           <TableCell className="max-w-xs truncate">{product.description}</TableCell>
-          <TableCell>${product.price.toFixed(2)}</TableCell>
+          <TableCell>${product.price?.toFixed(2)}</TableCell>
           <TableCell>{product.meassures}</TableCell>
-          <TableCell>{product.category}</TableCell>
+          <TableCell>{category.name}</TableCell>
           <TableCell>{product.qty}</TableCell>
+          <TableCell>{product.priority}</TableCell>
+          <TableCell>{product.is_published ? 'true' : 'false'}</TableCell>
           <TableCell>
             <Button
               variant="outline"
               className="mr-2"
-              onClick={() => onEdit(product)}>
-                Edit
+              onClick={() => handleEdit(product)}>
+              Edit
             </Button>
             <Button
               variant="destructive"
               onClick={() => handleRemoveProduct(product.id)}>
-                Remove
+              Remove
             </Button>
           </TableCell>
         </TableRow>
@@ -74,41 +80,57 @@ export function ProductsPanel() {
   };
 
   const renderCreateProductModal = () => (
-    <DialogContent className="sm:max-w-[450px] overflow-y-scroll max-h-[90%]">
-      <DialogHeader>
-        <DialogTitle>Add New Product</DialogTitle>
-      </DialogHeader>
-      <AddProductForm />
-    </DialogContent>
+    <Dialog open={showCreateProductModal} onOpenChange={setShowCreateProductModal}>
+      <DialogTrigger asChild>
+        <Button className="mb-4">Add Product</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[450px] overflow-y-scroll max-h-[90%]">
+        <DialogHeader>
+          <DialogTitle>Add New Product</DialogTitle>
+        </DialogHeader>
+        <ProductForm onSubmit={addProduct} />
+      </DialogContent>
+    </Dialog>
+  );
+
+  const renderEditProductModal = () => (
+    <Dialog open={showEditProductModal} onOpenChange={setShowEditProductModal}>
+      <DialogContent className="sm:max-w-[450px] overflow-y-scroll max-h-[90%]">
+        <DialogHeader>
+          <DialogTitle>Edit Product</DialogTitle>
+        </DialogHeader>
+        <ProductForm
+          onSubmit={editProduct}
+          initialValues={productToEdit} />
+      </DialogContent>
+    </Dialog>
   );
 
   return (
     <div>
-      <Dialog open={showCreateProductModal} onOpenChange={setShowCreateProductModal}>
-        <DialogTrigger asChild>
-          <Button className="mb-4">Add Product</Button>
-        </DialogTrigger>
-        {renderCreateProductModal()}
-      </Dialog>
+      {renderCreateProductModal()}
+      {renderEditProductModal()}
       <h2 className="text-2xl font-bold mb-4">Products</h2>
       <div className="overflow-x-auto">
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Image</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Description</TableHead>
-            <TableHead>Price</TableHead>
-            <TableHead>Measures</TableHead>
-            <TableHead>Category</TableHead>
-            <TableHead>Quantity</TableHead>
-            <TableHead>Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {renderProductsList()}
-        </TableBody>
-      </Table>
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Image</TableHead>
+              <TableHead>Name</TableHead>
+              <TableHead>Description</TableHead>
+              <TableHead>Price</TableHead>
+              <TableHead>Measures</TableHead>
+              <TableHead>Category</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Priority</TableHead>
+              <TableHead>Is published</TableHead>
+              <TableHead>Actions</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {renderProductsList()}
+          </TableBody>
+        </Table>
       </div>
     </div>
   )
