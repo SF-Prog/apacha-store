84f7877b31377e9de314dc6416a21f20aba0ef4b
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2025-04-24 00:34:56 -0300

    create wirkshop subscription action

---
+++ app/actions/create-workshop-subscription.ts
@@ -0,0 +1,28 @@
+import { supabase } from '@/lib/supabase/client';
+
+export async function createWorkshopSubscription(email: string) {
+  try {
+
+    const { error } = await supabase.from('subscriptions').insert({
+      email,
+      type: 'workshops'
+    });
+
+    if (error) {
+      return {
+        success: false,
+        error: error?.message ?? 'Failed to create workshop'
+      };
+    };
+
+    return {
+      success: true,
+      message: 'Workshop successfully created'
+    };
+  } catch (error) {
+    return {
+      success: false,
+      error: error?.message ?? 'Failed to create workshop'
+    }
+  }
+}
