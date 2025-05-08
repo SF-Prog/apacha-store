e8937fa13af356bd3b352fc98a5d654e496c0aab
Santiago Fitipaldo <fitipaldosantiago@gmail.com>
2025-05-08 13:56:04 -0300

    update footer

--- app/components/sections/footer/footer.tsx
+++ app/components/sections/footer/footer.tsx
@@ -4,7 +4,7 @@ export default function footer() {
   return (
     <footer className="bg-brown-800 py-8">
       <div className="container mx-auto px-8 text-center">
-        <p>&copy; 2024/25 Apacha Comida Saludable. Todos los derechos reservados.</p>
+        <p>&copy; 2024/25 Apacha Cocina Consciente. Todos los derechos reservados.</p>
       </div>
    </footer>
   );
